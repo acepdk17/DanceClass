c44060c87539b0b59a52f64348d63c92a051e929
Acep Dadang Koswara <93498663+acepdk17@users.noreply.github.com>
2022-01-10 16:19:24 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Welcome');
+});
+
+Route::get('/index', function () {
+    return view("Magazon/index");
+});
+
+Route::get('/blog', function () {
+    return view("Magazon/blog");
+});
+
+Route::get('/blog2', function () {
+    return view("Magazon/blog2");
+});
